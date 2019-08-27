42fb88dca92411a90eb32e2cf9527e8bb5917a75
moffrough <moffmu@gmail.com>
2019-08-27 11:16:15 +0300

    navbar fix

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,22 +1,37 @@
 <template>
-  <div class="container">
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">Portfolio</a>
-        <button class="button navbar-burger">
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
+  <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <img src="~/assets/imgs/mlogo.png" width="100px" height="100px" />
+      <h1 class="navbar-item">Moffhub Solutions</h1>
+      <a
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        @click="isOpen = !isOpen"
+        v-bind:class="{'is-active': isOpen}"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div class="navbar-menu" v-bind:class="{'is-active': isOpen}">
+      <div class="navbar-end">
+        <nuxt-link class="navbar-item" to="/">Home</nuxt-link>
+        <nuxt-link class="navbar-item" to="/about">About</nuxt-link>
+        <nuxt-link class="navbar-item" to="/projects">Projects</nuxt-link>
+        <nuxt-link class="navbar-item" to="/contact">Contact</nuxt-link>
       </div>
-      <div class="navbar-menu">
-        <div class="navbar-end">
-          <nuxt-link class="navbar-item" to="/">Home</nuxt-link>
-          <nuxt-link class="navbar-item" to="/about">About</nuxt-link>
-          <nuxt-link class="navbar-item" to="/projects">Projects</nuxt-link>
-          <nuxt-link class="navbar-item" to="/contact">Contact</nuxt-link>
-        </div>
-      </div>
-    </nav>
-  </div>
+    </div>
+  </nav>
 </template>
+<script>
+export default {
+  data: function() {
+    return {
+      isOpen: false
+    };
+  }
+};
+</script>
